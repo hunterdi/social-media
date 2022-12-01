c88d1dd07a6a2071a4f2e4665147a3ac31f0e8f7
Jurandir Santos <jurandir.santos@gylabs.com.br>
2022-12-01 17:42:03 +0000

    feat:added devcontainer

--- scripts/kafka/connect/start.sh
+++
@@ -1,4 +0,0 @@
-# setup connections
-curl -i -X POST -H "Accept:application/json" -H  "Content-Type:application/json" http://localhost:8083/connectors/ -d @connections/es-sink-users.json
-curl -i -X POST -H "Accept:application/json" -H  "Content-Type:application/json" http://localhost:8083/connectors/ -d @connections/es-sink-posts.json
-curl -i -X POST -H "Accept:application/json" -H  "Content-Type:application/json" http://localhost:8083/connectors/ -d @connections/timescale.json
--- scripts/keycloak/keycloak.dockerfile
+++
@@ -1,6 +0,0 @@
-FROM jboss/keycloak:latest
-
-# COPY ./scripts/keycloak/keycloak-entrypoint.sh /opt/jboss/tools/keycloak-entrypoint.sh
-# RUN chmod 777 /opt/jboss/tools/keycloak-entrypoint.sh
-
-# ENTRYPOINT [ "/opt/jboss/tools/keycloak-entrypoint.sh" ]
